fa95f466e6e3ea508925dd05354a363a07f323f8
vishnuwe45 <vishnu.prasad@we45.com>
2022-07-19 09:51:22 +0000

    message

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import json
+
+
+app = Flask(__name__)
+data =[]
+
+@app.route('/webhook', methods=['POST'])
+def index():
+    data1 = request.get_json(force=True)
+
+    data.append(data1)
+
+    return "sucess"
+
+
+@app.route('/listner', methods=['GET'])
+def data_listner():
+    if len(data)==0:
+        data.append({"data":"NO data found"})
+    json_formatted_str = json.loads(json.dumps(data[-1], indent=4))
+    return render_template('listner.html', data=json_formatted_str)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
